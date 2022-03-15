6e8680586d219df7923ebbd19353d3e9e5831ac0
TaePaper <taejeong055@gmail.com>
2022-03-15 21:37:23 +0900

    commit;

---
+++ codintTestPractice/src/Step3For/printdfd.java
@@ -0,0 +1,20 @@
+package Step3For;
+
+import java.util.Scanner;
+
+public class printdfd {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+		int T = 5;
+	
+		
+		
+		
+		for(int i =0; i<T; i++) {
+			System.out.println(A+B);
+		}
+
+	}
+
+}
